41382cb175c7112f068cd15fb1e34e79986496ea
r-keenan <ross.keenan@protonmail.com>
2021-02-17 20:45:37 -0600

    Finished form.

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ form.addEventListener("submit", (e) => {
   if (name.value === "" || name.value === null) {
     messages.push("Name is required");
   }
+  if (password.value.length <= 6) {
+    messages.push("Password must be longer than 6 characters");
+  }
+
+  if (password.value.length >= 20) {
+    messages.push("Password must be less than 20 characters");
+  }
+
+  if (password.value === "password") {
+    messages.push("Password cannot be password");
+  }
+
   if (messages.length > 0) {
     e.preventDefault();
     errorElement.innerText = messages.join(", ");
